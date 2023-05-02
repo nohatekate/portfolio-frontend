858fd6e9b8ee48eb7bd674e0468fdde40ea802e6
nohatekate <kateiscoding@gmail.com>
2023-05-02 14:25:06 -0500

    create components, import components and Route, and clean up App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+
+export default function App() {
+  const URL = "<http://localhost:4000/>";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/projects' element={<About URL={URL} />} />
+        <Route path='/about' element={<Projects URL={URL} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
-export default App;
