9630ec141924fa5019e5fa214a85ddb04e630a71
nohatekate <kateiscoding@gmail.com>
2023-05-07 18:13:07 -0500

    resume download

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import { Link } from 'react-router-dom';
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
-
 export default function Header(props) {
-
     return (
         <header>
             <nav>
@@ -12,9 +10,8 @@ export default function Header(props) {
                 <div className='nav-links'>
                     <Link to='/about'>about</Link>
                     <Link to='/projects'>projects</Link>
-                    <a className="icon"href='https://www.linkedin.com/in/kateiscoding/' target='_blank' rel="noreferrer">< FaLinkedin /></a>
-                    <a className="icon"href='https://github.com/nohatekate' target='_blank' rel="noreferrer">< FaGithub /></a>
-
+                    <a className="icon" href='https://www.linkedin.com/in/kateiscoding/' target='_blank' rel="noreferrer">< FaLinkedin /></a>
+                    <a className="icon" href='https://github.com/nohatekate' target='_blank' rel="noreferrer">< FaGithub /></a>
                 </div>
 
             </nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,21 @@
 import { useState, useEffect } from 'react';
+import { MdPictureAsPdf } from "react-icons/md";
 
 export default function About(props) {
+
+    const onButtonClick = () => {
+        fetch('/resume/KatherineResumePDF.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'KatherineResumePDF.pdf';
+                alink.click();
+            })
+        })
+    }
 
     const [about, setAbout] = useState(null);
 
@@ -25,7 +40,20 @@ export default function About(props) {
         return (
             <div className="about-page-content">
                 <h2>{about.name}</h2>
-                <h3><a href="mailto:kateiscoding@gmail.com">{about.email}</a></h3>
+
+
+                <div className='email-and-resume'>
+                    <div><p>Email:</p></div>
+                    <div><a href="mailto:kateiscoding@gmail.com">{about.email}</a></div>
+                </div>
+
+                <div className='email-and-resume'>
+                    <p>Resume:</p>
+                    <button className="icon" onClick={onButtonClick}><MdPictureAsPdf /></button>
+                </div>
+
+
+
 
                 <div className='about-details'>
 
